0b64098606e5893f33071460bf82978097208061
Mahuyon <latomv@gmail.com>
2024-01-31 00:37:15 -0800

    Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-
-This is a module that containts no class or attributes
-
-"""
+"""Defines a locked class."""
 
 
 class LockedClass:
-    __slots__ = ['first_name']
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
 
-    def __init__(self):
-        """ Init method """
-        pass
+    __slots__ = ["first_name"]
