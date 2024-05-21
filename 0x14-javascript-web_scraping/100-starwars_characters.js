76f0bc67f3121354d4c3b65774b2502ab3ef0664
Mahuyon <latomv@gmail.com>
2024-05-21 10:23:14 -0700

    Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/' + process.argv[2];
-
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const characters = resp.characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request.get(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  }
+  const resp = JSON.parse(body);
+  const charac = resp.characters;
+  for (const i of charac) {
+    request.get(i, function (err, res, body1) {
+      if (err) {
+        console.log(err);
+      }
+      const data = JSON.parse(body1);
+      console.log(data.name);
+    });
   }
 });
