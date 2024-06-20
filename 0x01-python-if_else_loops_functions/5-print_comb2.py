4ced75ef255ce1bc3358fe3ad472ec975fda7b20
Mahuyon <latomv@gmail.com>
2024-06-20 05:07:33 -0700

    Prints numbers from 0 to 99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for eya in range(0, 100):
+        if eya != 99:
+                print("{:02d}, ".format(eya), end='')
+        else:
+                print("{:02d}".format(eya))
